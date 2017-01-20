ebbeb23a60538ce705a455ceaee207d88c673131
Kira Parker <kiraclimber@gmail.com>
2017-01-20 15:48:31 -0700

    updated graph so it could be more complicated

--- MainSimulation.py
+++ MainSimulation.py
@@ -3,9 +3,10 @@ from Person import Person
 import sys
 
 class Graph:
-    def __init__(self,startNode,endNode):
+    def __init__(self,startNode,endNode, nodeList):
         self.startNode = startNode
         self.endNode = endNode
+        self.nodeList = nodeList #list of nodes from end to start
         endNode.graph = self
 
     def addPerson(self):
@@ -14,6 +15,9 @@ class Graph:
         self.startNode.addToQueue(p)
 
     def simulate(self,numPeople):
+        #start all the threads
+        for node in self.nodeList:
+            node.start()
         self.endNode.numPeople = numPeople
         for i in range(numPeople):
             self.addPerson()
@@ -21,24 +25,57 @@ class Graph:
         self.startNode.stop()
         sys.exit()
 
-def makeGraph(numIDcheckNodes, numScanners, numAITS):
+def getIndicesOfNum(num, twoDList):
+    indices = []
+    for i in range(0, len(twoDList)):
+        for j in range(0, len(twoDList[i])):
+            if(twoDList[i][j] == num):
+                indices.append(i)
+                break
+    return indices
+
+def makeGraph(numIDcheckNodes, numScanners, numAITS, dropOffForAITS, idCheckToDropoff): #dropOffForAITS is a 2-d list of length numScanners that says which AIT(s) the scanner goes to (0 goes to 0 (or more) always)
     def probFunction():
         return 0
+    # endNode = EndNode()
+    # pickUpNode = Node(2, probFunction, [endNode], 100, "pickUp")
+    # aitNode = Node(1.3, probFunction, [pickUpNode], 100, "ait")
+    # dropOffNode = Node(2, probFunction, [aitNode], 100, "dropOff")
+    # idCheckNode = Node(1.3, probFunction, [dropOffNode], 100, "idCheck")
+    # startNode = Node(0, probFunction, [idCheckNode], 100, "start")
+    # nodeList = [endNode, pickUpNode, aitNode, dropOffNode, idCheckNode, startNode]
+
     endNode = EndNode()
-    pickUpNode = Node(2, probFunction, [endNode], 100, "pickUp")
-    aitNode = Node(1.3, probFunction, [pickUpNode], 100, "ait")
-    dropOffNode = Node(2, probFunction, [aitNode], 100, "dropOff")
-    idCheckNode = Node(1.3, probFunction, [dropOffNode], 100, "idCheck")
-    startNode = Node(0, probFunction, [idCheckNode], 100, "start")
-    nodeList = [endNode, pickUpNode, aitNode, dropOffNode, idCheckNode, startNode]
+    pickUpNodeList = []
+    for i in range(numScanners):
+        pickUpNodeList.append(Node(2, probFunction, [endNode], 100, "pickUp"))
+    aitNodeList = []
+    for i in range(numAITS):
+        adjacencyList = []
+        for i in getIndicesOfNum(i, dropOffForAITS):
+            adjacencyList.append(pickUpNodeList[i])
+        aitNodeList.append(Node(1.3, probFunction, adjacencyList, 100, "ait"))
+    dropOffNodeList = []
+    for i in range(numScanners):
+        adjacencyList = []
+        for ait in dropOffForAITS[i]:
+            adjacencyList.append(aitNodeList[ait])
+        dropOffNodeList.append(Node(2, probFunction, adjacencyList, 20, "dropOff"))
+    idCheckNodeList = []
+    for i in range(numIDcheckNodes):
+        adjacencyList = []
+        for idCheck in idCheckToDropoff:
+            adjacencyList.append(dropOffNodeList[idCheck])
+        idCheckNodeList.append(Node(1.3, probFunction, adjacencyList, 1, "idCheck"))
+    startNode = Node(0, probFunction, idCheckNodeList, 100, "start")
 
-    for node in nodeList:
-        try:
-            node.start()
-        except(KeyboardInterrupt, SystemExit):
-            sys.exit()
+    nodeList = [endNode]
+    nodeList = nodeList + pickUpNodeList + aitNodeList + dropOffNodeList + idCheckNodeList
+    nodeList.append(startNode)
+    # for node in nodeList:
+    #     node.start()
 
-    g = Graph(startNode, endNode)
+    g = Graph(startNode, endNode, nodeList)
     g.simulate(1)
 
     # p = Person()
---
+++ testing.py
@@ -0,0 +1,5 @@
+a = [1,2]
+b = [2,3]
+print(a+b)
+a.extend(b)
+print(a)
