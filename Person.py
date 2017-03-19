2f8e39948cac3417a0f079b93146d00c488159f2
Kira Parker <kiraclimber@gmail.com>
2017-03-19 17:55:14 -0600

    code has been commented

--- Person.py
+++ Person.py
@@ -1,9 +1,11 @@
 import time
 scalar = 100
 
+#Represents a person that travels through the TSA security graph
 class Person:
-    counter = 1
+    counter = 1 #counts the number of people that have been created (static variable)
     def __init__(self):
+        """Creates a new person object"""
         self.timeSpent = 0
         self.timesAtNodes = []
         self.id = Person.counter
@@ -13,9 +15,11 @@ class Person:
         self.precheck = 0 #0 means not precheck, 1 means precheck
 
     def startWaiting(self):
+        """The person has started waiting in line at a node"""
         self.startTime = time.time()
 
     def endWaiting(self):
+        """The person is done waiting in line at that node (records how long they were there)"""
         self.endTime = time.time()
         diff = (self.endTime - self.startTime)*scalar
         self.timeSpent += diff
--- node.py
+++ node.py
@@ -4,10 +4,20 @@ import sys
 import threading
 scalar = 100
 count = 1
-#thread_trace = []
 
+#A class whose objects represent nodes in the TSA security graph. Extends threading.Thread because each node is its
+#own thread - this allows for real-time simulation.
 class Node(threading.Thread):
     def __init__(self, timeFunction, function, adjacencyList, defaultChoiceIndex, queueMax, name):
+        """
+        Creates a new node with the given parameters.
+        :param timeFunction: function that determines the amount of time a passenger spends at that node
+        :param function: function that determines which node the passenger will choose next
+        :param adjacencyList: list of all the nodes the passenger could choose to go next
+        :param defaultChoiceIndex: index of the passenger's default choice node (usually the one directly in front of them)
+        :param queueMax: maximum length of the queue at this node
+        :param name: name of the node
+        """
         threading.Thread.__init__(self, name=name)
         self.queue = []
         self.name = name
@@ -20,46 +30,51 @@ class Node(threading.Thread):
         self.shouldFinish = threading.Event()
         self.shouldWork = threading.Event()
 
-    def addToQueue(self, person):#add person to end of array, front of queue is front of array
+    def addToQueue(self, person):
+        """
+        Adds a person to the end of the list that is the queue at the node.
+        :param person: person to be added to the queue
+        :return: true if the person is successfully added, false otherwise (cannot be added if that would case the queue
+        to go over its maximum length)
+        """
         if (len(self.queue)>= self.queueMax ):
             return False
         
         person.queuesAtNodes.append(len(self.queue))
         self.queue.append(person)
         person.endWaiting()
-        #print("start", self.name, person.id)
         person.path.append(self.name)
         self.shouldWork.set()
         return True
-        #if(not(self.isSimulating))
 
     def startSimulation(self):
+        """
+        Starts simulating the flow of passengers through the node.
+        """
         # self.isSimulating = True
         while(len(self.queue)>0):
             person = self.queue.pop(0)
-            time.sleep(self.timeFunction(person.path))
-            #person.endWaiting()
+            time.sleep(self.timeFunction(person.path)) #wait for the person to finish doing their business at the front of the line
             queueLengths = list(map(lambda n: len(n.queue), self.adjacencyList))
 
-            nodeIndex = self.choiceFunction(queueLengths,self.defaultChoiceIndex, person.path)
+            nodeIndex = self.choiceFunction(queueLengths,self.defaultChoiceIndex, person.path) #index of the next node the passenger will go to
             while (True):
-                if (self.adjacencyList[nodeIndex].addToQueue(person)):
+                if (self.adjacencyList[nodeIndex].addToQueue(person)): #person successfully added to queue at their next node
                     person.startWaiting()
-                    #print("end", self.name, person.id)
                     break
-                # else:
-                #     print("person ", person.id, " is waiting at ", self.name, "the next queue has length: ", queueLengths[nodeIndex])
-
         # self.isSimulating = False
 
     def run(self):
+        """
+        Runs the thread essentially (and calls the simulation so it runs until the thread is exited)
+        """
         while not self.shouldFinish.is_set(): #so that program will end when you want it to
-            #thread_trace.append(self.name)#this will really cause it to slow down, but might give info
             self.shouldWork.wait()
             self.startSimulation()
             self.shouldWork.clear()
     
-    def stop(self): #ends the program
+    def stop(self):
+        """Ends the program"""
         for node in self.adjacencyList:
             node.stop()
         self.shouldWork.set() # just in case the thread is waiting at the time this is called
@@ -69,25 +84,30 @@ class Node(threading.Thread):
         return self.name
 
 
-
+#Represents the last node in the graph. When a person reaches this node, information about their path through security
+#is written to a csv file. When the last person reaches this node, the program terminates.
 class EndNode(Node):
     def __init__(self, filename):
+        """
+        Creates a new EndNode that will write to the given file.
+        :param filename: file the node will write its information to
+        """
         Node.__init__(self, 0, None, None, 0, 1000, "end")
         self.count = 0
         self.numPeople = 0
-        self.graph = None
+        self.graph = None #will get sets from inside the Graph class
         self.filename = filename
         self.startTime = 0
 
     def addToQueue(self, person):
+        """
+        Finishes a passenger's trip through security and writes out their data to a csv file.
+        :param person: person who has reached the end of security
+        :return: true (person is always added to the queue because they spend no time at this node so line never grows)
+        """
         import time
         global count #order in which people finish
         global scalar
-        person.endWaiting()
-        #print("Person", person.id,"finished:",person.timeSpent)
-        #print(person.path)
-        #print(person.queuesAtNodes)
-        #print(list(map(lambda x: round(x,2), person.timesAtNodes)))
         newPersonTimes = self.formatPerson(person.timesAtNodes, person.path)
         with open(self.filename, 'a') as peoplecsv:
             peoplecsv.write('{},{},{},{},'.format(count, (time.time()-self.startTime)*scalar, person.id,person.timeSpent,sep=','))
@@ -96,12 +116,18 @@ class EndNode(Node):
                 peoplecsv.write('{},'.format(time, sep=','))
             peoplecsv.write('{}\n'.format(person.precheck, sep=','))
         self.count += 1
-        if(self.count == self.numPeople):
+        if(self.count == self.numPeople): #everyone has gone through security and the program should terminate
             print("done")
             self.graph.finish()
         return True
 
     def formatPerson(self, timeAtNodeInfo, path):
+        """
+        Formats the data about a passenger's path through security so it looks nice in the csv file
+        :param timeAtNodeInfo: time the person spent at each node they passed through (list)
+        :param path: list of each node the passenger went through
+        :return: the formatted data
+        """
         newNodeInfo = timeAtNodeInfo.copy()
         del newNodeInfo[0]
         #see if it contains zoneDpatdown
@@ -116,14 +142,8 @@ class EndNode(Node):
 
 
     def stop(self):
+        """
+        Kills the thread that is this node.
+        """
         self.shouldWork.set()
         self.shouldFinish.set()
-
-
-# class StartNode(Node):
-#     def __init__(self, time, function, adjacencyList):
-#         Node.__init__(self, time, function, adjacencyList)
-#     def addToQueue(self, person):
-#         for node in self.adjacencyList:
-#             if(len(node.queue)==0):
-#                 node.addToQueue(person)
